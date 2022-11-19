84e3363798f633da61b3b8a293a9cc3ca7a92b6f
xiaoli <1998619xiaoli@gmail.com>
2022-11-19 16:52:08 +0100

    fix and test of optimization_step

--- main.py
+++ main.py
@@ -1,23 +1,53 @@
 
-import utils
 import numpy as np
+import copy
+
 from Data import Data
+from search import *
+from utils import *
 
 
 
 
 def main():
 
-    data : Data = Data("Dataset/tiny_test.in")
+    data = Data.Data("Dataset/tiny_test.in")
 
     print(data.matrix.shape, data.router_range)
     print(data.backbone_cost, data.router_cost, data.budget)
     print(data.initial_backbone)
     print(data.target_area)
-    print(data.matrix)
+    # print(data.matrix)
+    
+    data.random_init()
+    router_mask = data.initial_routers_placement()
+    print("STARTING CONFIGURATION:")
+    print_routers(data.matrix, data.router_list)
 
     
-
+    random_init = 10
+    max_step = 50
+    max_score = 0
+    best_routers_settings = None
+    i = 0
+    for j in range(random_init):
+        while i < 50:
+            improved = optimization_step(router_mask, data.matrix, data.router_list, data.router_range, Policy.BEST, verbose=True)
+            if improved == 0:
+                i += 1
+        temp_score = get_covered_cells(router_mask, data.matrix, data.router_range)
+        if temp_score == data.target_area :
+            max_score = temp_score
+            best_routers_settings = copy(data.router_list)
+            break
+        if temp_score > max_score:
+            max_score = temp_score
+            best_routers_settings = copy(data.router_list)
+        data.random_init()
+        router_mask = data.initial_routers_placement()
+            
+    print("FINAL CONFIGURATION: ")
+    print_routers(data.matrix, data.router_list)
 
 
 
--- search.py
+++ search.py
@@ -1,8 +1,12 @@
 import Data
 from enum import Enum
-from utils import *
 from utils import get_covered_cells
+from utils import print_routers
+from utils import get_points_around_router
+from utils import filter_non_target_points
 
+class NotValidPolicyExcception(Exception):
+    pass
 
 class Action(Enum):
     LEFT = (0, -1)
@@ -15,7 +19,7 @@ class Policy(Enum):
     BEST = 2
 
 def move(router: list,
-         action: Action):
+         action: Action)->list:
     """
     moves a router in the direction requested
     no check in the boundary, since routers are generated inside interested point,
@@ -26,13 +30,13 @@ def move(router: list,
         action (Action): action taken from Action
 
     Returns:
-        _type_: new coordinate for router
+        list: new coordinate for router
     """
     router[0] = router[0] + action.value[0]
     router[1] = router[1] + action.value[1]
     return router
     
-def greedy_move(actual_score, map_mask, building_matrix, router_list):
+def greedy_move(actual_score, map_mask, building_matrix, router_list)->set:
     """
     return the first move set found that improves the overall score
 
@@ -46,11 +50,21 @@ def greedy_move(actual_score, map_mask, building_matrix, router_list):
         set: move set <router_idx, action, score_achieved>
     """
     greedy_move_set = ()
+    boundary = map_mask.shape
     for i, router in enumerate(router_list):
         start_coord = [router[0], router[1]]
         map_mask[start_coord[0], start_coord[1]] = 0
         for action in Action:
             move(router, action)
+            # violating the boundary, should be reset
+            # print("check: router[0]: {} >= boundary[0]{} ? ".format(router[0], boundary[0]))
+            if router[0] >= boundary[0] or router[0] < 0:
+                router = [start_coord[0], start_coord[1]]
+                continue
+            # print("check: router[1]: {} >= boundary[1]{} ? ".format(router[1], boundary[1]))
+            if router[1] >= boundary[1] or router[1] < 0:
+                router = [start_coord[0], start_coord[1]]
+                continue
             map_mask[router[0], router[1]] = 1
             temp_score = get_covered_cells(map_mask, building_matrix, range)
             map_mask[router[0], router[1]] = 0
@@ -64,7 +78,7 @@ def greedy_move(actual_score, map_mask, building_matrix, router_list):
     
     return greedy_move_set
 
-def best_move(actual_score, map_mask, building_matrix, router_list):
+def best_move(actual_score, map_mask, building_matrix, router_list, range):
     """
     return the best move set of <router_idx, action, score> found
 
@@ -77,17 +91,29 @@ def best_move(actual_score, map_mask, building_matrix, router_list):
     Returns:
         set: move set <router_idx, action, score_achieved>
     """
-    per_action_score = {}
+    # per_action_score = {}
     best_move_set = ()
     best_score = actual_score
+    
+    boundary = map_mask.shape
     for i, router in enumerate(router_list):
         start_coord = [router[0], router[1]]
         map_mask[start_coord[0], start_coord[1]] = 0
         for action in Action:
             move(router, action)
+            
+            # violating the boundary, should be reset
+            # print("check: router[0]: {} >= boundary[0]{} ? ".format(router[0], boundary[0]))
+            if router[0] >= boundary[0] or router[0] < 0:
+                router = [start_coord[0], start_coord[1]]
+                continue
+            # print("check: router[1]: {} >= boundary[1]{} ? ".format(router[1], boundary[1]))
+            if router[1] >= boundary[1] or router[1] < 0:
+                router = [start_coord[0], start_coord[1]]
+                continue
             map_mask[router[0], router[1]] = 1
             temp_score = get_covered_cells(map_mask, building_matrix, range)
-            per_action_score[str((router,action))] = temp_score
+            # per_action_score[str((router,action))] = temp_score
             map_mask[router[0], router[1]] = 0
             router = [start_coord[0], start_coord[1]]
             if temp_score > best_score:
@@ -95,59 +121,89 @@ def best_move(actual_score, map_mask, building_matrix, router_list):
         map_mask[start_coord[0], start_coord[1]] = 1
     return best_move_set
     
-def optimization_step(map_mask, building_matrix, router_list, range, policy):
+def optimization_step(map_mask, building_matrix, router_list, range, policy, verbose=True):
     """
     first stupid implementation
+    
+    Calculate the global score, and chose the action that improves the overall score
+    
     Args:
         map_mask (np.array): the mask of routers in the matrix
         building_matrix (np.array): the matrix representation of the building
         router_list (list): the list of router coordinates
         range (int): the router coverage range
         policy (Policy): the applied policy to find the move
+        verbose (str): if True, the move taken and the difference about the score achieved is displayed
 
+    Raise:
+        NotValidPolicyExcception if the policy chosen is not a valid one, an exception is raised
     Returns:
-        _type_: new coordinate for router
+        int: improving score
     """
     score = get_covered_cells(map_mask, building_matrix, range)
-    # per_action_score = {}
-    # for router in router_list:
-    #     start_coord = [router[0], router[1]]
-    #     map_mask[start_coord[0], start_coord[1]] = 0
-    #     for action in Action:
-    #         move(router, action)
-    #         map_mask[router[0], router[1]] = 1
-    #         temp_score = get_covered_cells(map_mask, building_matrix, range)
-    #         per_action_score[str((router,action))] = temp_score
-    #         map_mask[router[0], router[1]] = 0
-    #         router = [start_coord[0], start_coord[1]]
-    #     map_mask[start_coord[0], start_coord[1]] = 1
-    
+
     if policy == Policy.BEST:
-        move = best_move(score, map_mask, building_matrix, router_list, range)
+        move_set = best_move(score, map_mask, building_matrix, router_list, range)
     elif policy == Policy.GREEDY:
-        move = greedy_move(score, map_mask, building_matrix, router_list, range)
-    chosen_router = router_list[move[0]]
-    
+        move_set = greedy_move(score, map_mask, building_matrix, router_list, range)
+    else:
+        raise NotValidPolicyExcception("Not a valid policy")
+    chosen_router = router_list[move_set[0]]
+
     map_mask[chosen_router[0], chosen_router[1]] = 0
-    move(chosen_router, move[1])
+    move(chosen_router, move_set[1])
     map_mask[chosen_router[0], chosen_router[1]] = 1
+    improved = move_set[2] - score
+    if(verbose):
+        print("OPTIMIZATION STEP:")
+        print("\tscore before: {}\n\tascore after: {}".format(score, move_set[2]))
+        print("\trouter: {}\t action:{}\n\tscore improved by: {}".format(move_set[0], move_set[1], improved))
+        print_routers(building_matrix, router_list)
+    return improved
         
-def optimization_step_2(routers, map_mask, range):
+def optimization_step_2(map_mask, building_matrix, router_list, range, policy, verbose=True):
     """
     second stupid implementation
+    
+    Uses the same idea from the first implementation, but if the local score is improved,
+    then also the global score is improved too.
     
     Args:
         map_mask (np.array): the mask of routers in the matrix
         building_matrix (np.array): the matrix representation of the building
         router_list (list): the list of router coordinates
         range (int): the router coverage range
+        policy (Policy): the applied policy to find the move
+        verbose (str): if True, the move taken and the difference about the score achieved is displayed
 
     Returns:
-        _type_: new coordinate for router
+        int : improving score
     """
     
-    score = get_covered_cells(routers, map_mask, range)
+    score = get_covered_cells(router_list, map_mask, range)
+    covered_dict = {}
     per_action_score = {}
     # todo
+    
+    for router_coords in zip(*router_list.nonzero()):
+        # compute points covered by the router
+        points_covered_by_router = get_points_around_router(
+            map_mask,
+            router_coords,
+            range
+        )
+        # filter points covered by walls and void cells
+        points_covered_by_router = filter_non_target_points(
+            building_matrix,
+            router_coords,
+            points_covered_by_router
+        )
+
+        for covered_cell in points_covered_by_router:
+            if covered_cell not in covered_dict:
+                covered_dict[covered_cell] = 1
+            else:
+                covered_dict[covered_cell] += 1
+    
     return per_action_score
         
