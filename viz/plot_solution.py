f6ea00dee792f0bd31c49e96172a01b99f1bc6cf
bendico765 <gianlucamorcaldi@gmail.com>
2022-12-07 20:23:34 +0100

    Fixed heatmap and added docstrings

--- viz/plot_solution.py
+++ viz/plot_solution.py
@@ -1,9 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import utils
-import pandas as pd
-import seaborn as sns
-
 
 def plot_solution(
 		building_matrix: np.array,
@@ -12,6 +9,15 @@ def plot_solution(
 		backbone_starting_point: tuple = (),
 		ax=None
 ):
+	"""
+	Plots the building planimetry and routers placement
+
+	:param building_matrix: array of arrays, the matrix describing the building (voids, targets, walls)
+	:param routers_placement: array of arrays, the matrix describing the placement of the routers inside the building
+	:param backbone_nodes: list of tuples, the list of nodes belonging to the backbone
+	:param backbone_starting_point: tuple, the backbone initial starting point coordinates
+	:param ax: axis on which place the plot
+	"""
 	# creating matrix for plot
 	plot_matrix = np.zeros(shape=building_matrix.shape)
 
@@ -47,6 +53,14 @@ def plot_heatmap(
 	router_radius: int,
 	ax=None
 ):
+	"""
+	Plots a heatmap where lighter areas corresponds to cells covered by the routers
+
+	:param building_matrix: array of arrays, the matrix describing the building (voids, targets, walls)
+	:param routers_placement: array of arrays, the matrix describing the placement of the routers inside the building
+	:param router_radius: int, the range of a router
+	:param ax: axis on which place the plot
+	"""
 	# finding all the points covered by the routers
 	points_covered_by_routers = []
 	for router_coords in np.transpose(np.nonzero(routers_placement)):
@@ -64,29 +78,28 @@ def plot_heatmap(
 		for covered_point in covered_points:
 			points_covered_by_routers.append(covered_point)
 
-	covered_points_df = pd.DataFrame(
-		[
-			[point_x, point_y]
-			for point_x, point_y in points_covered_by_routers
-		],
-		columns=["row", "column"]
-	)
-	plot_solution(
-		building_matrix,
-		routers_placement,
-		ax=ax
-	)
-	sns.kdeplot(
-		data=covered_points_df,
-		x="column",
-		y="row",
-		cmap="inferno",
-		fill=True,
-		thresh=0.7,
-		alpha=0.75,
-		ax=ax
-	)
+	# creating matrix for plot
+	plot_matrix = np.zeros(shape=building_matrix.shape)
 
+	n, m = building_matrix.shape
+	for i in range(n):
+		for j in range(m):
+			# filling the matrix with voids/walls/targets
+			if building_matrix[i][j] == "#":  # adding wall
+				plot_matrix[i][j] = 10
+			elif building_matrix[i][j] == "-":  # adding void
+				plot_matrix[i][j] = 20
+
+			# adding routers
+			if routers_placement[i][j] == 1:
+				plot_matrix[i][j] = 30
+
+	# adding coverage points
+	for (i, j) in points_covered_by_routers:
+		if plot_matrix[i][j] == 0 or plot_matrix[i][j] == 20:
+			plot_matrix[i][j] = 40
+
+	ax.matshow(plot_matrix)
 
 def plot_complete(
 		building_matrix: np.array,
@@ -95,8 +108,20 @@ def plot_complete(
 		backbone_nodes: list = [],
 		backbone_starting_point: tuple = (),
 ):
+	"""
+	Plots the building planimetry (and routers placement) alongside the covered area of the building
+
+	:param building_matrix: array of arrays, the matrix describing the building (voids, targets, walls)
+	:param routers_placement: array of arrays, the matrix describing the placement of the routers inside the building
+	:param router_radius: int, the range of a router
+	:param backbone_nodes: list of tuples, the list of nodes belonging to the backbone
+	:param backbone_starting_point: tuple, the backbone initial starting point coordinates
+	"""
+	# creating figure with 2 subplots
 	fig, (ax1, ax2) = plt.subplots(1, 2)
-	fig.suptitle('Horizontally stacked subplots')
+	fig.suptitle("Results")
+
+	# plotting the solution
 	plot_solution(
 		building_matrix,
 		routers_placement,
@@ -104,10 +129,13 @@ def plot_complete(
 		backbone_starting_point,
 		ax1
 	)
+
+	# plotting the coverage of the solution
 	plot_heatmap(
 		building_matrix,
 		routers_placement,
 		router_radius,
 		ax2
 	)
+
 	plt.show()
